ec3491dbf15d4e5356c053ce6d5aef091481db65
frankleysantos <frankleysilvasantos@gmail.com>
2022-06-21 20:52:20 -0300

    trabalhando com messagens

--- agenda/settings.py
+++ agenda/settings.py
@@ -127,3 +127,15 @@ MEDIA_URL = 'media/'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# importar mensagens 
+from django.contrib.messages import constants
+
+# cria uma lista para alterar o padrão de mensagens do django usando as classes do bootstrap
+MESSAGE_TAGS = {
+    constants.ERROR: 'alert-danger', 
+    constants.WARNING: 'alert-warning', 
+    constants.DEBUG: 'alert-primary', 
+    constants.INFO: 'alert-info', 
+    constants.SUCCESS: 'alert-success', 
+}
--- contatos/views.py
+++ contatos/views.py
@@ -1,9 +1,10 @@
 from django.http import Http404
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
 from .models import Contato
 from django.core.paginator import Paginator # paginação
 from django.db.models import Q, Value #Usado para realizar consultas mais complexas
 from django.db.models.functions import Concat # concatenação de campos
+from django.contrib import messages
 # Create your views here.
 def index(request):
     contatos = Contato.objects.order_by('-id').filter(
@@ -30,8 +31,10 @@ def ver_contato(request, contato_id):
 def busca(request):
     termo = request.GET.get('termo')
 
-    if termo is None:
-        raise Http404() # raise levanta o erro nesse caso
+    if termo is None or not termo:
+        # raise Http404() # raise levanta o erro nesse caso
+        messages.add_message(request, messages.ERROR, 'Campo termo não pode ser vazio')
+        return redirect('index')
 
     campos = Concat('nome', Value(' '), 'sobrenome') # campos existentes base dados
     # contatos = Contato.objects.order_by('-id').filter(
