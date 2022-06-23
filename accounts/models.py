e719f4e4a42bb1350da5f2f2babc61d470a9d769
frankleysantos <frankleysilvasantos@gmail.com>
2022-06-23 11:52:13 -0300

    personalização de formularios

--- accounts/models.py
+++ accounts/models.py
@@ -4,6 +4,10 @@ from django import forms
 # Create your models here.
 
 class FormContato(forms.ModelForm):
+    # nome = forms.CharField(widget=forms.TextInput(attrs={'class': 'form-control'}))
+    # sobrenome = forms.CharField(widget=forms.TextInput(attrs={'class': 'form-control'}))
+    # descricao = forms.CharField(widget=forms.Textarea(attrs={'class': 'form-control'}))
+
     class Meta:
         model = Contato
         exclude = ('mostrar', 'data_criacao',)
